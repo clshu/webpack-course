0d8d93a54beaeff61d6a589f8567179a32f8f494
clshu <ccss0630@yahoo.com>
2019-12-30 18:23:03 -0800

    Section 2-7. @babel/preset-env and @babel/plugin-transform-runtime Finished 2-7 that I didn't finished in 2-7 branch. I followed the instruction but always encountere the same error. Redo the babel installtion with babel 7.7 (It was 6.x) It solved the problem with cleaner code in main.js. No require("babel-runtime/regenerator")

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ require('./main.css')
 require('./images/link.jpg')
 require('./index.html')
 
-const a = async () => {
-  await console.log('Hello Babel!')
+const a = async args => {
+  const { x, y } = args
+  await console.log(`Hello Babel! ${x} ${y}`)
   console.log('More')
 }
+
+a({ x: 1, y: 2 })
